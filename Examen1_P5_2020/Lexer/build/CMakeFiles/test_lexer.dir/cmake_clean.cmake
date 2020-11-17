6867298e9ed3653b152d38f491309fbd7f8d2fb5
fernando <isaiasm-hn>
2020-11-16 18:38:20 -0600

    Cmake listo

---
+++ Examen1_P5_2020/Lexer/build/CMakeFiles/test_lexer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_lexer.dir/expr_lexer.cpp.o"
+  "CMakeFiles/test_lexer.dir/test_lexer.cpp.o"
+  "test_lexer"
+  "test_lexer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_lexer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
